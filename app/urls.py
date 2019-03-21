1fa81f1a72ea66320ec1e1e6c9993119da7f8115
Hojjat Rafat <35579662+Hojjatrt@users.noreply.github.com>
2019-03-22 01:24:28 +0430

    add data view for json response

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('game/', views.game, name='game'),
+    path('data/', views.data, name='data'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import *
 
 
 def index(request):
@@ -10,3 +11,14 @@ def index(request):
 def game(request):
     print('game')
     return render(request, 'app/graph.html', {})
+
+
+def data(request):
+    graph = Graph.objects.first()
+    nodes = []
+    for i in graph.nodes.all():
+        nodes.append({'name': i.name, 'num': i.num, 'x': i.x, 'y': i.y})
+    links = []
+    for i in graph.links.all():
+        links.append({'num': i.num, 'source': i.source.num, 'target': i.target.num, 'weight': i.weight})
+    return JsonResponse({'nodes': nodes, 'links': links})
